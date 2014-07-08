f6690b942c5ff16dbafc0411bbde714dffdfc4e4
Hugo Silva <hugo.f.v.silva@gmail.com>
2014-07-08 10:33:50 +0100

    Fixed chrome with new_arch variable

--- scripts/background.js
+++ scripts/background.js
@@ -1,20 +1,14 @@
-var unbabel_url = "https://www.unbabel.co/stream/";
-var notification_id = "";
+var unbabel_url = "https://www.unbabel.com/streamv2/simple/";
+var notification_id = "show_paid_tasks";
 
 
-// setInterval(function(){
-//  chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-//  chrome.browserAction.setBadgeText({text: 'new'});
-// },3000);
-
-
-/* For demonstration purposes, the notification creation 
+/* For demonstration purposes, the notification creation
  * is attached to the browser-action's `onClicked` event.
  * Change according to your needs. */
 setInterval(function(){
     $.ajax({
         type : "GET",
-        url: "https://www.unbabel.co/api/v1/tasksavailable/?limit=1&paid=True",
+        url: "https://www.unbabel.com/api/v1/tasksavailable/?limit=1&paid=True&new_arch=True",
         dataType: "json",
         success: function(response){
             if(response.meta.total_count > 0){
@@ -25,7 +19,7 @@ setInterval(function(){
                         "title"         : "Hi there!",
                         "message"       : "There are new paid tasks available!",
                         "contextMessage": "www.unbabel.com"
-    
+
                     }, function(id) {
                         console.log("Create notification with ID: "+ notification_id);
                     });
@@ -40,7 +34,7 @@ setInterval(function(){
                     "title"         : "Sorry for that...",
                     "message"       : "To receive notifications you must login at unbabel.",
                     "contextMessage": "www.unbabel.com"
-    
+
                 }, function(id) {
                     console.log("Create notification with ID: "+ notification_id);
                 });
@@ -51,11 +45,11 @@ setInterval(function(){
 //}, 10000);
 }, 1200000);
 
-/* Add this to also handle the user's clicking 
+/* Add this to also handle the user's clicking
  * the small 'x' on the top right corner */
 //chrome.notifications.onClosed.addListener(function() {});
 
-chrome.browserAction.onClicked.addListener(function(tab){ 
+chrome.browserAction.onClicked.addListener(function(tab){
     openTab();
 });
 
